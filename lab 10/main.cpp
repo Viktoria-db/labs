66e326afc2a0308bc7e7eba3e9bc488c34bb4818
viktoriadb <viktoria@MacBook-Air-Viktoria.local>
2024-12-21 11:16:59 +0200

    1212

--- lab 10/main.cpp
+++ lab 10/main.cpp
@@ -1,110 +1,75 @@
-#include "func.hpp"
-#include <fstream>
-#include <sstream>
 #include <iostream>
-#include <set>
+#include <list>
 #include <algorithm>
-#include <cctype>
-#include <iterator>
-#include <map>
+#include <cmath>
 
+using namespace std;
 
-void findLongestWords(const std::string& filename, char letter, int N, const std::string& outputFilename) {
-    std::ifstream inputFile(filename);
-    std::ofstream outputFile(outputFilename);
-    std::string word;
-    std::set<std::string> uniqueWords;
-
-
-    while (inputFile >> word) {
-
-        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
-
-        if (word[0] == letter) {
-            uniqueWords.insert(word);
-        }
-    }
-
-
-    std::vector<std::string> sortedWords(uniqueWords.begin(), uniqueWords.end());
-    std::sort(sortedWords.begin(), sortedWords.end(), [](const std::string& a, const std::string& b) {
-        return a.size() > b.size();
-    });
-
-
-    for (int i = 0; i < std::min(N, (int)sortedWords.size()); ++i) {
-        outputFile << sortedWords[i] << "\n";
-    }
-}
-
-
-bool isPalindrome(const std::string& word) {
-    int left = 0;
-    int right = word.size() - 1;
-    while (left < right) {
-        if (word[left] != word[right]) return false;
-        ++left;
-        --right;
+// Функция для проверки, является ли число простым
+bool isPrime(int number) {
+    if (number <= 1) return false;
+    for (int i = 2; i <= sqrt(number); i++) {
+        if (number % i == 0) return false;
     }
     return true;
 }
 
-
-void processTextWithPalindromes(const std::string& text) {
-    std::istringstream stream(text);
-    std::string word;
-    std::vector<std::string> words;
-    std::map<char, int> vowelCount;
-
-
-    for (char c : text) {
-        if (std::isalpha(c)) {
-            char lowerChar = std::tolower(c);
-            if (lowerChar == 'a'  lowerChar == 'e'  lowerChar == 'i'  lowerChar == 'o'  lowerChar == 'u') {
-                vowelCount[lowerChar]++;
-            }
-        }
+// Функция проверки, начинается ли число с цифры 3
+bool startsWithThree(int number) {
+    while (number >= 10) {
+        number /= 10; // Убираем последнюю цифру
     }
-
-
-    while (stream >> word) {
-        if (word.size() > 2 && isPalindrome(word)) {
-            words.push_back(word);
-        }
-    }
-
-
-    if (!words.empty()) {
-        for (const std::string& w : words) {
-            std::cout << w << "\n";
-        }
-    } else {
-        std::cout << "No palindromes found.\n";
-    }
+    return number == 3; // Сравниваем первую цифру
 }
 
+int main() {
+    list<int> sequence; // Односвязный список
+    int n;
 
-void findWordsWithUniqueLetters(const std::string& filename, const std::string& outputFilename) {
-    std::ifstream inputFile(filename);
-    std::ofstream outputFile(outputFilename);
-    std::string word;
-
-
-    while (inputFile >> word) {
-        std::set<char> uniqueLetters;
-        for (char c : word) {
-            if (std::isalpha(c)) {
-                uniqueLetters.insert(std::tolower(c));
-            }
-        }
-
-
-        if (uniqueLetters.size() > 7) {
-            outputFile << word << " (";
-            for (const char& c : uniqueLetters) {
-                outputFile << c;
-            }
-            outputFile << ")\n";
+    // Ввод последовательности натуральных чисел
+    cout << "Введите последовательность натуральных чисел (завершите ввод 0):" << endl;
+    while (cin >> n && n != 0) {
+        if (n > 0) { // Добавляем натуральные числа
+            sequence.push_back(n);
         }
     }
+
+    // Проверяем, есть ли числа, начинающиеся с цифры 3
+    bool hasThree = false;
+    for (int number : sequence) {
+        if (startsWithThree(number)) {
+            hasThree = true;
+            break;
+        }
+    }
+
+    if (hasThree) {
+        // Если числа с первой цифрой 3 есть:
+        // Удаляем четные числа, кроме числа 2
+        sequence.remove_if([](int num) {
+            return (num % 2 == 0 && num != 2);
+        });
+
+        // Дублируем простые числа
+        for (auto it = sequence.begin(); it != sequence.end(); ++it) {
+            if (isPrime(*it)) {
+                it = sequence.insert(it, *it); // Дублируем значение
+                ++it; // Переходим к следующему элементу
+            }
+        }
+    } else {
+        // Если чисел с первой цифрой 3 нет:
+        // Сортируем по невозрастанию последней цифры
+        sequence.sort([](int a, int b) {
+            return (a % 10) > (b % 10); // Сравниваем последние цифры
+        });
+    }
+
+    // Вывод обработанной последовательности
+    cout << "Обработанная последовательность:" << endl;
+    for (int number : sequence) {
+        cout << number << " ";
+    }
+
+    return 0;
 }
